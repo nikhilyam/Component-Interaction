ae141ce0e3770f0c0801ea8a573d46bb49d47c24
nikhilyam <nik.yamsalwar@gmail.com>
2022-02-02 07:30:56 +0530

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroChildComponent } from './hero-child.component';
+import { HeroParentComponent } from './hero-parent.component';
 import { AstronautComponent } from './astronaut.component';
 
 @NgModule({
   imports: [BrowserModule],
-  declarations: [AppComponent, AstronautComponent],
+  declarations: [
+    AppComponent,
+    HeroParentComponent,
+    HeroChildComponent,
+    AstronautComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
